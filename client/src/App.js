409bfe991018b083d1f1faaccd8f79db4339dd5d
TrevorCollins <tcollinst1i@gmail.com>
2019-04-18 19:39:16 -0500

    react_components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import User from './components/User';
+import Plan from './components/Plan';
+import Timeline from './components/Timeline';
+import Event from './components/Event';
+import Meal from './components/Meal';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Router>
+          <Header />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/user' component={User} />
+          <Route exact path='/plan' component={Plan} />
+          <Route exact path='/timeline' component={Timeline} />
+          <Route exact path='/event' component={Event} />
+          <Route exact path='/meal' component={Meal} />
+        </Router>
+      </div>
     );
   }
 }
