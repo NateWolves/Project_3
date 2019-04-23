99bdf243fd1351db9bd55d1fc932c8d4b3c5c5a7
crsimpson5 <crsimpson5@gmail.com>
2019-04-22 19:35:25 -0500

    Added user login

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navbar from "../Navbar";
+import Auth from "../../utils/Auth";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div id="home">
-            <button>Plan A Trip</button>
-        </div>
-    )
+  return (
+    <div id="home">
+      <Navbar />
+      <div className="mt-5">
+        <Link to="/user">User page</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
