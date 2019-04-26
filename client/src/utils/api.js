0c58d8a7e121b4823f69838cbfe6332adb6800e4
crsimpson5 <crsimpson5@gmail.com>
2019-04-25 19:55:06 -0500

    fix

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -9,13 +9,13 @@ export default {
   findAllEvents: function(userId) {
     return axios.get(`/api/events/${userId}`);
   },
-  createEvent: function(userId) {
-    return axios.post(`/api/events/${userId}`);
+  createEvent: function(userId, data) {
+    return axios.post(`/api/events/${userId}`, data);
   },
-  updateEvent:  function(userId, eventId) {
-    return axios.put(`/api/events/${userId}/${eventId}`);
+  updateEvent:  function(userId, eventId, data) {
+    return axios.put(`/api/events/${userId}/${eventId}`, data);
   },
-  deleteEvent: function(userId, eventId) {
-    return axios.delete(`/api/events/${userId}/${eventId}`);
+  deleteEvent: function(userId, eventId, data) {
+    return axios.delete(`/api/events/${userId}/${eventId}`, data);
   }
 }
