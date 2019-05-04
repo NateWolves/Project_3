4224e27dc240909265504b35cdffd0c3a7f9e289
crsimpson5 <crsimpson5@gmail.com>
2019-05-04 13:22:28 -0500

    Edit trip modal added

--- client/src/pages/Trips/index.js
+++ client/src/pages/Trips/index.js
@@ -3,25 +3,32 @@ import moment from "moment";
 import { Link } from "react-router-dom";
 import { Container, Table } from "react-bootstrap"
 import Plan from "../../components/Plan";
+import EditTripModal from "../../components/EditTripModal";
 
 import API from "../../utils/api";
+import EditEventModal from "../../components/EditTripModal";
 
 class Trips extends Component {
   state = {
-    modalShow: false,
+    addModalShow: false,
+    editModalShow: false,
     trips: [],
-    userId: ""
+    userId: "",
+    tripId: "",
+    tripName: "",
+    tripStartDate: "",
+    tripEndDate: ""
   };
 
   handleAddTripClick = () => {
     this.setState({
-      modalShow: true
+      addModalShow: true
     });
   };
 
   handleSubmitTrip = tripObj => {
     this.setState({
-      modalShow: false
+      addModalShow: false
     });
     API.createTrip(tripObj)
       .then(res => {
@@ -31,12 +38,28 @@ class Trips extends Component {
       });
   };
 
+  handleEditTripClick = tripObj => {
+    this.setState({
+      tripId: tripObj._id,
+      tripName: tripObj.name,
+      tripStartDate: tripObj.startDate,
+      tripEndDate: tripObj.endDate
+    }, () => {
+      this.setState({
+        editModalShow: true
+      });
+    });
+  };
+
   handleEditTrip = tripObj => {
     let newTrips = this.state.trips.filter(trip => trip._id !== tripObj._id)
-    
+    newTrips.push(tripObj)
+
     this.setState({
-      trips: newTrips.push(tripObj)
+      editModalShow: false,
+      trips: newTrips
     });
+
     API.updateTrip(tripObj._id, tripObj);
   };
 
@@ -49,7 +72,8 @@ class Trips extends Component {
 
   modalClose = () => {
     this.setState({
-      modalShow: false
+      addModalShow: false,
+      editModalShow: false
     });
   };
 
@@ -81,7 +105,7 @@ class Trips extends Component {
         </Container>
         <Plan
           onHide={this.modalClose}
-          show={this.state.modalShow}
+          show={this.state.addModalShow}
           handleSubmit={this.handleSubmitTrip}
           userId={this.state.userId}
         />
@@ -92,38 +116,47 @@ class Trips extends Component {
                 <th>Trip</th>
                 <th>Start Date</th>
                 <th>End Date</th>
-                <th>Remove</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
-            {this.state.trips.map(trip => {
-              return (
-                <tr key={`trip-${trip._id}`}>
-                  <td>
-                    <Link to={`/trips/${trip._id}`}>
-                      <h2>{trip.name}</h2>
-                    </Link>
-                  </td>
-                  <td>
-                    {moment(trip.startDate).format("M/D")}
-                  </td>
-                  <td>
-                    {moment(trip.endDate).format("M/D")}
-                  </td>
-                  <td>
-                    <button onClick={() => {
-                      return this.handleDeleteTrip(trip._id);
-                    }}>
-                      Remove Trip
+              {this.state.trips.map(trip => {
+                return (
+                  <tr key={`trip-${trip._id}`}>
+                    <td>
+                      <Link to={`/trips/${trip._id}`}>
+                        <h2>{trip.name}</h2>
+                      </Link>
+                    </td>
+                    <td>
+                      {moment(trip.startDate).format("dddd - MMMM Do")}
+                    </td>
+                    <td>
+                      {moment(trip.endDate).format("dddd - MMMM Do")}
+                    </td>
+                    <td>
+                      <button onClick={() => this.handleEditTripClick(trip)}>
+                        Edit
                     </button>
-                  </td>
-                </tr>
-              );
-            })}
+                      <button onClick={() => this.handleDeleteTrip(trip._id)}>
+                        Remove Trip
+                    </button>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </Table>
         </Container>
-        
+        <EditEventModal
+          onHide={this.modalClose}
+          show={this.state.editModalShow}
+          handleEditTrip={this.handleEditTrip}
+          tripId={this.state.tripId}
+          name={this.state.tripName}
+          startDate={this.state.tripStartDate}
+          endDate={this.state.tripEndDate}
+        />
       </Fragment>
     );
   }
