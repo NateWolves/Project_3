bd0821be179273fed8fa5443ae10d9cceb6187f3
Curtis Simpson <crsimpson5@gmail.com>
2019-05-08 15:08:56 -0500

    server/package.json

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
+require("dotenv").config();
 const express = require("express");
+const session = require('express-session')
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const cors = require('cors');
+const cookieParser= require('cookie-parser');
+const logger = require('morgan');
+const passport = require('passport')
+
+const corsOption = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+};
+
 // Define middleware here
+app.use(cors(corsOption));
+
+app.use(logger('dev'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+// Passport.js setup
+app.use(session({
+  secret: 'my-secret',
+  resave:false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session())
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -16,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", { useNewUrlParser: true, useCreateIndex: true });
 
 // Start the API server
 app.listen(PORT, function() {
