137400304a8be011440b94d6f4ad0adee3aedc96
Curtis Simpson <crsimpson5@gmail.com>
2019-05-08 15:46:17 -0500

    merge

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> b
